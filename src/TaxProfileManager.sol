b9a3db7871efdb0321141060a1812f78fb032c91
Joe <joe.andrew.bennett@gmail.com>
2023-11-29 20:31:35 -0500

    cleanup, profile consolidate

--- src/TaxProfileManager.sol
+++ src/TaxProfileManager.sol
@@ -1,36 +1,20 @@
  // SPDX-License-Identifier: UNLICENSED 
 pragma solidity 0.8.19;
  
-
 //depreciation -> business owner. Claim car
 //write off based on depreciation 
 //business owner, not 
+//state
 //different assets classes future readme
-//
+//zillow data maybe?
  contract TaxProfileManager{
     uint public taskCount = 0;
-    mapping(uint => Task) public tasks;
+    mapping(uint => UserProfile) public userProfiles;
     
-    struct Task{
+    struct UserProfile{
         uint id;
-        string content;
-        bool completed;
-    }
-    
-    constructor() public{
-        createTask("Like, link, and subscribe!");
-    }
-
-    function createTask(string memory _content) public{
-        taskCount++;
-        tasks[taskCount] = Task(taskCount, _content, false);
-        emit TaskCreated(taskCount, _content, false);
-    }
-
-    function toggleCompleted(uint _id) public {
-        Task memory _task = tasks[_id];
-        _task.completed = !_task.completed;
-        tasks[_id] = _task;
-        emit TaskCompleted(_id, _task.completed);
+        string walletAddress;
+        bool businessOwner;
+        string state;
     }
 }
