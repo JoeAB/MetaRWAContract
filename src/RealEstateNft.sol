be8399eea6cc5570d87956a4e9463a97acefce33
Joe <joe.andrew.bennett@gmail.com>
2023-12-10 11:08:25 -0500

    fixing constructor for contract

--- src/RealEstateNft.sol
+++ src/RealEstateNft.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RealEstateNFT is ERC721Enumerable, Ownable {
-    constructor() ERC721("RealEstateNFT", "RE") {}
+    constructor() ERC721("RealEstateNFT", "RE") Ownable(msg.sender) {}
 
     function mint(address to, uint256 tokenId) external onlyOwner {
         _mint(to, tokenId);
