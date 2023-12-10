f28b6cccebd88d44d29394a6f7aeeabc2d316545
Joe <joe.andrew.bennett@gmail.com>
2023-12-10 15:19:47 -0500

    update to take URL we stream to binary data

--- src/RealEstateNft.sol
+++ src/RealEstateNft.sol
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 
 contract RealEstateNFT is ERC721Enumerable, Ownable {
     constructor() ERC721("RealEstateNFT", "RE") Ownable(msg.sender) {}
 
-    function mint(address to, uint256 tokenId) external onlyOwner {
+    function mint(address to, string memory tokenUrl) external onlyOwner {
+        bytes32 hash = keccak256(bytes(tokenUrl));
+        uint256 tokenId = uint256(hash);
         _mint(to, tokenId);
     }
 }
