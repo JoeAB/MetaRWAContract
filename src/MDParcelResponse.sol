e685e7cfcb1b7d84d757c5b5e21d275e5f33f81a
Joe <joe.andrew.bennett@gmail.com>
2023-12-10 07:30:27 -0500

    Updates to functional contract using chainlink functions

--- src/MDParcelResponse.sol
+++ src/MDParcelResponse.sol
@@ -1,83 +1,98 @@
- // SPDX-License-Identifier: UNLICENSED 
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
-import "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/FunctionsClient.sol";
+import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/libraries/FunctionsRequest.sol";
 
-/**
- * Request testnet LINK and ETH here: https://faucets.chain.link/
- * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
- */
+contract MdParcelResponse is FunctionsClient, ConfirmedOwner {
+    using FunctionsRequest for FunctionsRequest.Request;
 
-contract MDParcelResponse is ChainlinkClient, ConfirmedOwner {
-    using Chainlink for Chainlink.Request;
+    bytes32 public s_lastRequestId;
+    bytes public s_lastResponse;
+    bytes public s_lastError;
+    string private s_lastSearchTerm;
 
-    // variable bytes(arbitrary-length raw byte data) returned in a single oracle response
-    bytes public data;
-    string public address_json;
+    error UnexpectedRequestID(bytes32 requestId);
 
-    bytes32 private jobId;
-    uint256 private fee;
 
-    /**
-     * @notice Initialize the link token and target oracle
-     * @dev The oracle address must be an Operator contract for multiword response
-     *
-     *
-     * Sepolia Testnet details:
-     * Link Token: 0x779877A7B0D9E8603169DdbD7836e478b4624789
-     * Oracle: 0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD (Chainlink DevRel)
-     * jobId: 7da2702f37fd48e5b1b9a5715e3509b6
-     *
-     */
-    constructor() ConfirmedOwner(msg.sender) {
-        setChainlinkToken(0x779877A7B0D9E8603169DdbD7836e478b4624789);
-        setChainlinkOracle(0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD);
-        jobId = "7da2702f37fd48e5b1b9a5715e3509b6";
-        fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
+    Property[] Properties;
+    
+    struct Property{
+        string searchTerm;
+        string valuationLabel;
     }
 
-    /**
-     * @notice Request variable bytes from the oracle
-     */
-    function requestBytes(string memory parcelAddress) public {
-        string memory requestPath = string.concat("https://geodata.md.gov/imap/rest/services/PlanningCadastre/MD_PropertyData/MapServer/0/query?where=address='",parcelAddress,"'&outFields=*&outSR=4326&f=json");
-        Chainlink.Request memory req = buildChainlinkRequest(
-            jobId,
-            address(this),
-            this.fulfillBytes.selector
+    event Response(bytes32 indexed requestId, bytes response, bytes err);
+    event PropertyAdded(string searchTerm, string valuationLabel);
+    event LogEntered(string searchTerm);
+    event LogRequestSent(bytes32 indexed requestId, string searchTerm);
+    event LogFulfillment(bytes32 indexed requestId, string searchTerm, string valuationLabel);
+    event LogError(bytes32 indexed requestId, string error);
+
+    //Sepolia Router address;
+    address router = 0xb83E47C2bC239B3bf370bc41e1459A34b41238D0;
+
+    //Functions Subscription ID
+    uint64 subscriptionId = 1824; 
+
+    //Gas limit for callback tx do not change
+    uint32 gasLimit = 300000;
+
+    //DoN ID for Sepolia, from supported networks in the docs
+    bytes32 donId =
+        0x66756e2d657468657265756d2d7365706f6c69612d3100000000000000000000;
+
+    constructor() FunctionsClient(router) ConfirmedOwner(msg.sender) {}
+
+    function sendRequest(string memory addressLookUp) external onlyOwner returns (bytes32 requestId) {
+
+        emit LogEntered(addressLookUp);
+        // Source JavaScript to run
+        string memory source =
+            string.concat("const url = `https://geodata.md.gov/imap/rest/services/PlanningCadastre/MD_PropertyData/MapServer/0/query?where=address='", addressLookUp,"'&outFields=*&outSR=4326&f=json`; const newRequest = Functions.makeHttpRequest({ url }); const newResponse = await newRequest; if (newResponse.error) { throw Error(`Error fetching news`); } return Functions.encodeString(JSON.stringify(newResponse.data.features[0].attributes.NFMIMPVL));");
+
+        FunctionsRequest.Request memory req;
+        req.initializeRequestForInlineJavaScript(source);
+
+        s_lastRequestId = _sendRequest(
+            req.encodeCBOR(),
+            subscriptionId,
+            gasLimit,
+            donId
         );
-        req.add(
-            "get",
-            requestPath
-        );
-        req.add("path", "features");
-        sendChainlinkRequest(req, fee);
+        s_lastSearchTerm = addressLookUp;
+        
+        emit LogRequestSent(s_lastRequestId, s_lastSearchTerm);
+
+        return s_lastRequestId;
     }
 
-    event RequestFulfilled(bytes32 indexed requestId, bytes indexed data);
-
-    /**
-     * @notice Fulfillment function for variable bytes
-     * @dev This is called by the oracle. recordChainlinkFulfillment must be used.
-     */
-    function fulfillBytes(
+    function fulfillRequest(
         bytes32 requestId,
-        bytes memory bytesData
-    ) public recordChainlinkFulfillment(requestId) {
-        emit RequestFulfilled(requestId, bytesData);
-        data = bytesData;
-        address_json = string(data);
+        bytes memory response,
+        bytes memory err
+    ) internal override {
+        if (s_lastRequestId != requestId) {
+            emit LogError(requestId, "UnexpectedRequestID");
+            revert UnexpectedRequestID(requestId);
+        }
+        Properties.push(Property(s_lastSearchTerm, string(response)));
+        emit PropertyAdded(s_lastSearchTerm, string(response));
+        s_lastResponse = response;
+
+        emit LogFulfillment(requestId, s_lastSearchTerm, string(response));
+
+        s_lastError = err;
+        emit Response(requestId, s_lastResponse, s_lastError);
     }
 
-    /**
-     * Allow withdraw of Link tokens from the contract
-     */
-    function withdrawLink() public onlyOwner {
-        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
-        require(
-            link.transfer(msg.sender, link.balanceOf(address(this))),
-            "Unable to transfer"
-        );
+    // Function to return all results
+    function getSearchResult() public view returns (string[] memory) {
+        string[] memory allProperties = new string[](Properties.length);
+        for (uint i = 0; i < Properties.length; i++) {
+            allProperties[i] = string.concat(Properties[i].searchTerm, ": ",Properties[i].valuationLabel);
+        }
+        return allProperties;
     }
 }
